fe911399b110d4b12d8f7e1222594e562f310678
BillDunlap <williamwdunlap@gmail.com>
2024-12-10 10:31:35 -0800

    Set PID params at runtime (e.g., via AdvantageScope)

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -10,6 +10,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 // import com.kauailabs.navx.AHRSProtocol;
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.SPI;
@@ -17,34 +20,52 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // import frc.robot.subsystems.OmniWheel; // no need to import class in same directory
 import frc.robot.Constants;
-import frc.robot.TuningVariables;
+// import frc.robot.TuningVariables;
 
 public class DriveTrain extends SubsystemBase {
   private final OmniWheel m_front;
   private final OmniWheel m_right;
   private final OmniWheel m_left;
+  private double m_kF;
+  private double m_kP;
   private final AHRS m_ahrs;
 
   /** Creates a new DriveTrain. */
   public DriveTrain() {
     m_front = new OmniWheel(1, "front", Constants.kWheelDiameterFt);
-    SendableRegistry.setName(m_front, getSubsystem(), "front wheel");
-    SendableRegistry.addChild(this, m_front);
-    SmartDashboard.putData(m_front);
+    //SmartDashboard.putData(m_front);
     m_right = new OmniWheel(2, "right", Constants.kWheelDiameterFt);
-    addChild("DriveTrain.addChild: RIGHT", m_right);
-    SmartDashboard.putData(m_right);
+    //SmartDashboard.putData(m_right);
     m_left = new OmniWheel(3, "left", Constants.kWheelDiameterFt);
-    addChild("LeFt", m_left);
-    SmartDashboard.putData("m_left", m_left);
+    //SmartDashboard.putData("m_left", m_left);
     m_ahrs = new AHRS(SPI.Port.kMXP);
-    addChild("navx", m_ahrs);
     SendableRegistry.setName(m_ahrs, "DriveTr", "gyro");
     SmartDashboard.putData("Gyro", m_ahrs);
     // The AHRS has not settled down on an angle yet when
     // the constructor for drive train is called.
     setZeroAngleDegrees(0.0); // assume 'front' wheel points 'ahead'
+    setKF(1.15);
+    setKP(0.750);
     beStill();
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder){
+    builder.addDoubleProperty("kF", () -> m_kF, this::setKF);
+    builder.addDoubleProperty("kP", () -> m_kP, this::setKP);
+  }
+
+  private void setKF(double kF){
+    m_kF = kF;
+    m_front.setKF(m_kF);
+    m_left.setKF(m_kF);
+    m_right.setKF(m_kF);
+  }
+  private void setKP(double kP){
+    m_kP = kP;
+    m_front.setKP(m_kP);
+    m_left.setKP(m_kP);
+    m_right.setKP(m_kP);
   }
 
   public void setZeroAngleDegrees(double degrees) {
@@ -144,17 +165,10 @@ public class DriveTrain extends SubsystemBase {
 
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    if (TuningVariables.debugLevel.get() >= 3) {
-      double angle = m_ahrs.getAngle();
-      SmartDashboard.putNumber("Angle", angle);
-    }
-  }
+  public void periodic() {}
   private static class OmniWheel extends SubsystemBase {
     private final TalonSRX m_talonsrx;
     private double m_fractionPower;
-    private final String m_name;
     private final double m_diameter;
      // The triomnidrive robot has US Digital E4P-360-250 encoders.
      //    360 cycles (== 1440 clicks) per revolution and hole for quarter inch shaft.
@@ -162,6 +176,13 @@ public class DriveTrain extends SubsystemBase {
     private final double m_nativeUnitsOverRevolutionsPerSecond;
     private final double m_nativeUnitsOverFeetPerSecond;  
     private double m_kF;
+    private double m_kP;
+    private double m_targetClicksPer100Ms;
+    private double m_currentClicksPer100Ms;
+    private double m_errorClicksPer100Ms;
+    private StructArrayPublisher<Translation2d> publisher = 
+      NetworkTableInstance.getDefault()
+      .getStructArrayTopic("Volts,Speed", Translation2d.struct).publish();
     /** Creates a new OmniWheel, controlled by a TalonSRX motor controller
      *  @param canID:  CAN bus identifier for the motor controller
      *  @param name: a name for the wheel to be used in SmartDashboard displays
@@ -169,8 +190,8 @@ public class DriveTrain extends SubsystemBase {
      */ 
     OmniWheel(int canId, String name, double diameter) {
       m_talonsrx = new TalonSRX(canId);
+      setName(name);
       m_fractionPower = 0;
-      m_name = name;
       m_diameter = diameter;
       m_talonsrx.configFactoryDefault();
       m_talonsrx.clearStickyFaults();
@@ -187,8 +208,8 @@ public class DriveTrain extends SubsystemBase {
       // requested speed was 150 or 200.
       // The above was written by me a few years ago.  It is wrong.  I should
       // figure out a better (bigger, I think - units are Volts/SpeedUnit) kF and set kI to 0.
-      setKF(1.15);
-      m_talonsrx.config_kP(Constants.TalonSRX.kDriveTrainPIDLoopIndex, 1.2);
+      setKF(0.0);
+      setKP(0.0);
       m_talonsrx.config_kI(Constants.TalonSRX.kDriveTrainPIDLoopIndex, 0.0);
       m_talonsrx.config_kD(Constants.TalonSRX.kDriveTrainPIDLoopIndex, 0.0); 
       SmartDashboard.putData(this); 
@@ -198,9 +219,13 @@ public class DriveTrain extends SubsystemBase {
      * set feed forward parameter.
      * @param kF feed forward parameter.  Units are Volts per Clicks/100Milliseconds.
      */
-    private void setKF(double kF){
+    public void setKF(double kF){
       m_kF = kF;
       m_talonsrx.config_kF(Constants.TalonSRX.kDriveTrainPIDLoopIndex, m_kF); 
+    }
+    public void setKP(double kP){
+      m_kP = kP;
+      m_talonsrx.config_kP(Constants.TalonSRX.kDriveTrainPIDLoopIndex, m_kP);
     }
  
     /** Set the speed of the wheel on a scale of-1 to 1. 
@@ -208,13 +233,14 @@ public class DriveTrain extends SubsystemBase {
           0 means stopped.
         */
     public void setFractionPower(double fractionPower) {
+      m_targetClicksPer100Ms = -1;  // code for don't care
       m_fractionPower = fractionPower;
       m_talonsrx.set(ControlMode.PercentOutput,m_fractionPower);
     }
 
     public void setVelocityRevolutionsPerSecond(double revolutionsPerSecond){
-      double clicksPer100Ms = revolutionsPerSecond * m_nativeUnitsOverRevolutionsPerSecond;
-      setVelocityNativeUnits(clicksPer100Ms);
+      double targetClicksPer100Ms = revolutionsPerSecond * m_nativeUnitsOverRevolutionsPerSecond;
+      setVelocityNativeUnits(targetClicksPer100Ms);
     }
     public void setVelocityFeetPerSecond(double feetPerSecond){
       double clicksPer100Ms = feetPerSecond * m_nativeUnitsOverFeetPerSecond;
@@ -222,22 +248,33 @@ public class DriveTrain extends SubsystemBase {
     }
 
     /** set target velocity in "native" units: ticks per 100 milliseconds */
-    public void setVelocityNativeUnits(double clicksPer100Ms) {
-      m_talonsrx.set(ControlMode.Velocity, clicksPer100Ms);
+    public void setVelocityNativeUnits(double targetClicksPer100Ms) {
+      m_targetClicksPer100Ms = targetClicksPer100Ms;
+      m_talonsrx.set(ControlMode.Velocity, m_targetClicksPer100Ms);
     }
 
     @Override
     public void periodic() {
-      // perhaps use this to collect sensor information to avoid double collection.
+      // use this to collect sensor information to avoid double collection.
+      m_currentClicksPer100Ms = m_talonsrx.getSelectedSensorVelocity();
+      m_errorClicksPer100Ms = m_targetClicksPer100Ms - m_currentClicksPer100Ms;
+      publisher.set(new Translation2d[]{new Translation2d(m_talonsrx.getMotorOutputVoltage(), m_currentClicksPer100Ms)});
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
-      super.initSendable(builder); // is this needed? or desirable?
+      // super.initSendable(builder); // add .hasDefault, .default, .hasCommand, and .command properties
       builder.addDoubleProperty("percentPower", () -> m_talonsrx.getMotorOutputPercent(), null);
-      builder.addDoubleProperty("velocity", () -> m_talonsrx.getSelectedSensorVelocity(), null);
-      builder.addDoubleProperty("integral accumulation",() -> m_talonsrx.getIntegralAccumulator(), null);
-      builder.addDoubleProperty("kF", () -> m_kF, this::setKF);
+      builder.addDoubleProperty("motorVoltage", () -> m_talonsrx.getMotorOutputVoltage(), null);
+      builder.addDoubleProperty("target velocity (native units)", () -> m_targetClicksPer100Ms, null);
+      builder.addDoubleProperty("vel. error (native units)", () -> m_errorClicksPer100Ms, null);
+      builder.addDoubleProperty("velocity (native units)", () -> m_currentClicksPer100Ms, null);
+      builder.addDoubleProperty("V_over_sp", () -> { return Math.abs(m_talonsrx.getSelectedSensorVelocity())>5
+          ? m_talonsrx.getMotorOutputVoltage()/m_talonsrx.getSelectedSensorVelocity()
+          : 0.0;}, null);
+      //builder.addDoubleProperty("integral accumulation",() -> m_talonsrx.getIntegralAccumulator(), null);
+      builder.addDoubleProperty("kF", () -> m_kF, null);
+      builder.addDoubleProperty("kP", () -> m_kP, null);
       // builder.addDoubleProperty("setpoint", this::getSetpoint, this::setSetpoint);     builder.addDoubleProperty("velocity", m_talonsrx.getSelectedSensorVelocity());
     }
   }
